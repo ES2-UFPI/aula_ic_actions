52a1b6fab8b92527ae522ce41acd626c3b97bda9
Guilherme Avelino <gavelino@gmail.com>
2024-12-05 09:39:33 -0300

    minor change in app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Aula GitHub actions
+          Aula GitHub actions with Continuous deploy
         </p>
         <a
           className="App-link"
