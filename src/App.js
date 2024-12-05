d7b9163ae19f3587f7b1008b5a574c9fab7443f6
Guilherme Avelino <gavelino@gmail.com>
2024-12-05 09:49:46 -0300

    fix issue x

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Aula GitHub actions with Continuous deploy
+          Aula GitHub actions with Continuous deploy. Fixed bug task1
         </p>
         <a
           className="App-link"
